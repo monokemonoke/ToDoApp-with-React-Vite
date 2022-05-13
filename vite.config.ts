42556548fd2e4e29aee288148edc685e122ae201
川畑竜平 <kawabataryuuhei@kawabataryuuheinoMacBook-Pro.local>
2022-05-13 17:12:56 +0900

    add sample icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: '../public',
-  },
-  root: './src',
-  plugins: [react()]
-})
+	server: {
+		open: true,
+	},
+	build: {
+		outDir: '../public',
+	},
+	root: './src',
+	plugins: [react({ jsxImportSource: '@emotion/react' })],
+});
