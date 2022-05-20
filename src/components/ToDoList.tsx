199351966778d3ddc0169e1898db21f8f01add7d
川畑竜平 <kawabataryuuhei@kawabataryuuheinoMacBook-Pro.local>
2022-05-20 17:31:04 +0900

    add update feature

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -28,6 +28,13 @@ const TodoList = () => {
         setTodos(todos => [...todos, { task, isCompleted: false }]);
         setTask('');
     }
+    const handleUpdateTask = (index: number) => {
+        const newTodos = todos.map((todo, todoIndex) => {
+            if (todoIndex === index) todo.isCompleted = !todo.isCompleted;
+            return todo;
+        });
+        setTodos(newTodos);
+    }
     const handleRemoveTask = (index: number) => {
         const newTodos = [...todos];
         newTodos.splice(index, 1);
@@ -50,7 +57,7 @@ const TodoList = () => {
                 {todos.map((todo, index) => (
                     <li key={index}>
                         {todo.task}
-                        <button>{todo.isCompleted ? "Fin" : "WIP"}</button>
+                        <span onClick={() => handleUpdateTask(index)}>{todo.isCompleted ? "✅" : "⬜"}</span>
                         <span onClick={() => handleRemoveTask(index)}>X</span>
                     </li>
                 ))}
